c961810326c3f939bcd9ae6116d6a0ba3334e600
Giseudo Oliveira <giseudo@gmail.com>
2017-03-07 12:40:36 -0300

    Added cheap-module-source-map to docs build devtool

--- build/webpack/prod-docs.js
+++ build/webpack/prod-docs.js
@@ -3,10 +3,11 @@ import merge from 'webpack-merge';
 import baseWebpackConfig from './base';
 
 export default merge(baseWebpackConfig, {
+	devtool: 'cheap-module-source-map',
 	output: {
 		path: './dist',
 		publicPath: '',
-		filename: '[name].js',
-		chunkFilename: '[name].js'
+		filename: '[name].bundle.js',
+		chunkFilename: '[name].bundle.js'
 	}
 });
