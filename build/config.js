a80e3be09e05ea45e8bb7f914773533c6dc8b3de
Giseudo Oliveira <giseudo@gmail.com>
2017-03-07 11:55:30 -0300

    Removed folders

--- build/config.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-
-export default {
-	rootPath: path.join(__dirname, '../dist'),
-	publicPath: '/',
-	indexPath: 'docs/index.html',
-	assetsPath: 'docs/src/assets',
-	docsPath: 'docs'
-};
--- src/directives/index.js
+++
@@ -1,5 +0,0 @@
-import LoadMoreDirective from './load-more/load-more.directive.vue';
-
-export {
-	LoadMoreDirective
-};
